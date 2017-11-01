79ab78bc4781360e9e81902fa8f0532aaf73b43c
SiuKim <siukim1611@gmail.com>
2017-11-01 22:16:27 +0800

    Booking Query Added

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Manager/JDBCBookingManager.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Manager/JDBCBookingManager.java
@@ -3,6 +3,7 @@ package com.example.bot.spring;
 import lombok.extern.slf4j.Slf4j;
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.nio.channels.SelectableChannel;
 import java.sql.*;
 import java.util.ArrayList;
 import java.net.URISyntaxException;
@@ -12,7 +13,52 @@ import java.util.*;
 
 
 public class JDBCBookingManager extends bookingManager {
-	public ArrayList<Booking> getBookingByLineUserId ( String LineUserId) { return null ; }  
+
+	/**
+	 *
+	 * @param LineUserId : Line ID to search
+	 * @return the result array list of Booking
+     */
+	public ArrayList<Booking> getBookingByLineUserId ( String LineUserId) {
+		String SQLstatement = " SELECT * FROM Booking WHERE LineUserId = " + LineUserId;
+		ResultSet rs = SelectionQuery(SQLstatement);
+		ArrayList<Booking> result = new ArrayList<Booking>();
+		try {
+			while (rs.next()) {
+				int noOfAdults, noOfChildern, noOfToodler;
+				double tourFee, amountPaid, serviceCharge;
+				boolean confirmed;
+				String date, customerID, bookingID, tourID, specialRequest;
+
+				bookingID = rs.getString("BookTableID");
+				customerID = rs.getString("CUSTOMER ID");
+				date = rs.getString("Date");
+				tourID = rs.getString("Tour ID");
+				specialRequest = rs.getString("Special Request");
+
+				noOfAdults = rs.getInt("Number of Adults");
+				noOfChildern = rs.getInt("Number of Children");
+				noOfToodler = rs.getInt("Number of Toodler");
+
+				tourFee = rs.getDouble("Tour Fee");
+				amountPaid = rs.getDouble("Amount Paid");
+				serviceCharge = rs.getDouble("service charge");
+
+				confirmed = rs.getBoolean("confirmed");
+
+
+				Booking booking = new Booking(bookingID);
+				result.add(booking);
+			}
+		}catch (SQLException e){
+
+		}
+
+
+
+
+		return null ;
+	}
 	public void insertBooking(Booking booking) {}; 
 	public void deleteBookingByLineIdAndTourName ( String lineId , String tourName ) {}
 
