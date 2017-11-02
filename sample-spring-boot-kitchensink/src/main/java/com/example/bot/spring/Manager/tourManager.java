6771c492677829593fd0c4605e308f95be9fff19
SiuKim <siukim1611@gmail.com>
2017-11-02 18:50:02 +0800

    Function overloading

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Manager/tourManager.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Manager/tourManager.java
@@ -12,8 +12,10 @@ import java.util.*;
 
 public abstract class tourManager extends Manager {
 	public abstract ArrayList<Tour> getToursByPriceRange( int low , int high);
-	public abstract ArrayList<Tour> getToursByPriceRange( int low , int high , ArrayList<Tour> tours) ; 
-	public abstract ArrayList<Tour> getTourByPlace( String place , ArrayList<Tour> tours) ; 
+	public abstract ArrayList<Tour> getToursByPriceRange( int low , int high , ArrayList<Tour> tours) ;
+	public abstract ArrayList<Tour> getTourByPlace( String place) ;
+	public abstract ArrayList<Tour> getTourByPlace( String place , ArrayList<Tour> tours) ;
+	public abstract ArrayList<Tour> getToursByTime( String startTime , String endTime ) ;
 	public abstract ArrayList<Tour> getToursByTime( String startTime , String endTime , ArrayList<Tour> tours) ;
 	public abstract void insertTour(Tour tour);
 }
