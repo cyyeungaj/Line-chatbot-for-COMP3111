797cc6f7e7fc4cc3b81ed9753c5b29278d6f6b1f
cyyeungaj <cyyeungaj@connect.ust.hk>
2017-11-02 23:03:35 +0800

    'add

---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Manager/faqManager.java
@@ -0,0 +1,16 @@
+package com.example.bot.spring;
+
+import lombok.extern.slf4j.Slf4j;
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.net.URISyntaxException;
+import java.io.IOException;
+import java.net.URI;
+import java.util.*;
+
+
+public abstract class faqManager extends Manager{
+	public abstract ArrayList<FAQ> getAllFAQ();
+	public abstract boolean insertFAQ(FAQ faq);
+}
