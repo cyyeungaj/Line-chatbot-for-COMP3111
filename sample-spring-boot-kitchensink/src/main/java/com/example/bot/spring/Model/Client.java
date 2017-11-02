5fc23debef10ed2b6313ff767f25c00454d2b64c
Lam Man Yiu <puoklam0@gmail.com>
2017-11-02 17:49:18 +0800

    adding Client Model

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Model/Client.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Model/Client.java
@@ -1,5 +1,20 @@
 package com.example.bot.spring;
 
 public class Client {
-
+	private String clientID;
+	private String name;
+	private int phoneNumber;
+	private int age;
+	
+	public Client(String clientID, String name, int phoneNumber, int age) {
+		this.clientID = clientID;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.age = age;
+	}
+	
+	public String getClientID() {return clientID;}
+	public String getName() {return name;}
+	public int getPhoneNumber() {return phoneNumber;}
+	public int getAge() {return age;}
 }
