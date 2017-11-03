3bfcd06348a3d21a9911b4f80ce40e51c7c09e89
Lam Man Yiu <puoklam0@gmail.com>
2017-11-03 13:23:51 +0800

    fix sync of customertTable in database and Client Model

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Interface/BookingInterface/BookingInterface.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Interface/BookingInterface/BookingInterface.java
@@ -1,20 +1,22 @@
 package com.example.bot.spring;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class BookingInterface extends UserInterface {
 	
-	private final static int INITIAL_STATE = 0 ; 
-	private final static int ASK_FOR_TOUR_STATE = 1 ;
-	private final static int ASK_FOR_TOUR_CONFIRM_STATE = 2 ; 
-	private final static int ASK_FOR_DATE_STATE = 3 ; 
-	private final static int ASK_FOR_ADULTSNO_STATE = 4 ; 
-	private final static int ASK_FOR_TOODLERNO_STATE = 5 ; 
-	private final static int ASK_FOR_FEECONFIRM_STATE = 6 ; 
-	private final static int SHOW_ASSEMBLY_POINT_STATE = 7 ; 
+	private final static int INITIAL_STATE = 1 ; 
+	private final static int ASK_FOR_TOUR_STATE = 2 ;
+	private final static int ASK_FOR_TOUR_CONFIRM_STATE = 3 ; 
+	private final static int ASK_FOR_DATE_STATE = 4 ; 
+	private final static int ASK_FOR_ADULTSNO_STATE = 5 ; 
+	private final static int ASK_FOR_TOODLERNO_STATE = 6 ; 
+	private final static int ASK_FOR_FEECONFIRM_STATE = 7 ; 
+	private final static int SHOW_ASSEMBLY_POINT_STATE = 8 ; 
 	private int currentState ;
 	
 	public BookingInterface ( ) {
-		 
+		
 	}
 	
 	public void processInput( chatbotController controller, String userReply) {
--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Model/Client.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Model/Client.java
@@ -2,14 +2,12 @@ package com.example.bot.spring;
 
 public class Client {
 
-	private String lineUserId ; 
 	private String cutstomerId;
 	private String name;
 	private int phoneNumber;
 	private int age;
 	
-	public Client(String lineUserId , String cutstomerId, String name, int phoneNumber, int age) {
-		this.lineUserId = lineUserId ; 
+	public Client(String cutstomerId, String name, int phoneNumber, int age) {
 		this.cutstomerId = cutstomerId;
 		this.name = name;
 		this.phoneNumber = phoneNumber;
