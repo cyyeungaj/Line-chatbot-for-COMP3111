5b098e18430c3e4d6c3493f98a673940b3f368e3
ckong2001 <kaminglo2001@gmail.com>
2017-11-04 03:59:22 +0800

    test point

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Interface/FAQInterface/FAQInterface.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Interface/FAQInterface/FAQInterface.java
@@ -92,9 +92,9 @@ public class FAQInterface extends UserInterface {
 
 	public FAQInterface () {
 		currentState = SHOW_FAQ_STATE;
-		//super.setMessage(getFAQList()) ; 
+		super.setMessage(new JDBCFaqManager().getAllFAQString()) ; 
 		
-		super.setMessage("test") ; 
+		//super.setMessage("test") ; work 
 		
 		//lineMessagingClient.pushMessage(new PushMessage("U2336052073d2a3192d088b3215554ee1", new TextMessage("testing for push")));
 	}
